9332ad8b0781d19931e3d20b17f45d7b27ebda04
Malte <malte.roevemeier@ge.com>
2024-07-17 14:13:29 +0200

    created tableOnMain Branch

--- backend/databaseTemperature.php
+++ backend/databaseTemperature.php
@@ -4,18 +4,21 @@ $db_user = "beispiel_user";
 $db_pass = "beispiel_pass";
 $dbName = "temphumidity";
 $charset = "utf8mb4";
-$reset_time_1 = new DateTime("02:00:00");
-$reset_time_2 = new DateTime("02:10:00");
 
 $dsn = "mysql:host=$dbServer;dbname=$dbName;charset=$charset";
 $opt = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ];
-$pdoTemp = new PDO($dsn, $db_user, $db_pass, $opt);
-unset($dbServer, $db_user, $db_pass, $dbName, $charset);
+try {
+    $pdoTemp = new PDO($dsn, $db_user, $db_pass, $opt);
+    unset($dbServer, $db_user, $db_pass, $dbName, $charset);
 
-$sql = "DELETE FROM temphumidity WHERE stamp < NOW() - INTERVAL 1 MONTH";
-    
-$stmt = $pdoTemp->prepare($sql);
-$stmt->execute();
+    $sql = "DELETE FROM temphumidity WHERE stamp < NOW() - INTERVAL 1 MONTH";
+    $stmt = $pdoTemp->prepare($sql);
+    $stmt->execute();
+} catch (PDOException $e) {
+    die("Datenbankverbindung fehlgeschlagen: " . $e->getMessage());
+}
+?>
--- mainTemp.php
+++ mainTemp.php
@@ -2,28 +2,11 @@
 $faviconPath = "images/png/logo.png";
 include 'backend/databaseTemperature.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['dId'])) {
-    $deviceId = $_POST['dId'];
-    $posX = isset($_POST['pos_x']) ? $_POST['pos_x'] : null;
-    $posY = isset($_POST['pos_y']) ? $_POST['pos_y'] : null;
-
-    if ($posX !== null && $posY !== null) {
-        $query = "UPDATE buttonposition SET pos_x = :pos_x, pos_y = :pos_y WHERE device_ID = :device_ID";
-        $stmt = $pdoTemp->prepare($query);
-        $stmt->bindParam(':pos_x', $posX, PDO::PARAM_INT);
-        $stmt->bindParam(':pos_y', $posY, PDO::PARAM_INT);
-        $stmt->bindParam(':device_ID', $deviceId, PDO::PARAM_STR);
-
-        if ($stmt->execute()) {
-        }
-    }
-}
-
 $temperatures = [];
+$device_ids = [];
 
 $deviceQuery = "SELECT DISTINCT device_id FROM temphumidity";
 $deviceStmt = $pdoTemp->prepare($deviceQuery);
-$device_ids = [];
 
 if ($deviceStmt->execute()) {
     while ($row = $deviceStmt->fetch(PDO::FETCH_ASSOC)) {
@@ -39,7 +22,6 @@ if ($missingPosStmt->execute()) {
     $row = $missingPosStmt->fetch(PDO::FETCH_ASSOC);
     $missingPosCount = $row['count'];
 }
-
 
 foreach ($device_ids as $device_id) {
     $query = "SELECT temp FROM temphumidity WHERE device_id = :device_id ORDER BY stamp DESC LIMIT 1";
@@ -58,14 +40,7 @@ foreach ($device_ids as $device_id) {
         }
 
         $temperatures[$device_id] = ['temp' => $temp, 'colorClass' => $colorClass];
-    } else {
-        $temperatures[$device_id] = ['temp' => 'Error', 'colorClass' => 'buttonError'];
     }
-    
-    $insertQuery = "INSERT INTO buttonposition (device_id) VALUES (:device_id) ON DUPLICATE KEY UPDATE device_id = device_id";
-    $insertStmt = $pdoTemp->prepare($insertQuery);
-    $insertStmt->bindParam(':device_id', $device_id);
-    $insertStmt->execute();
 }
 
 $buttonPositions = [];
@@ -88,49 +63,45 @@ if ($positionStmt->execute()) {
     <link rel="stylesheet" href="assets/styleTemp.css">
     <link rel="icon" href="<?php echo $faviconPath; ?>" type="image/x-icon">
 </head>
-<body class="with-background" onclick="showClickPosition(event)">
+<body class="with-background">
     <div class="all">
-    <img src="/images/png/FertigungNeu.png" style="position : absolute; top: 0;">
-
-    <div class="container">
-        <?php foreach ($temperatures as $device_id => $data): ?>
-            <?php 
-            $pos_x = isset($buttonPositions[$device_id]['pos_x']) ? $buttonPositions[$device_id]['pos_x'] : null;
-            $pos_y = isset($buttonPositions[$device_id]['pos_y']) ? $buttonPositions[$device_id]['pos_y'] : null;
-            if ($pos_x !== null && $pos_y !== null && $device_id !== null): ?>
-                <form method="get" action="tempValues.php" class="sensor-form" style="position: absolute; left: <?php echo $pos_x - 52.5; ?>px; top: <?php echo $pos_y - 125; ?>px;">
-                    <button class="button <?php echo $data['colorClass']; ?>" type="submit" name="sensor" value="<?php echo htmlspecialchars($device_id); ?>">
+        <img src="/images/png/FertigungNeu.png" style="position: absolute; top: 0;">
+        <div class="container">
+            <?php foreach ($temperatures as $device_id => $data): ?>
+                <?php 
+                $pos_x = isset($buttonPositions[$device_id]['pos_x']) ? $buttonPositions[$device_id]['pos_x'] : null;
+                $pos_y = isset($buttonPositions[$device_id]['pos_y']) ? $buttonPositions[$device_id]['pos_y'] : null;
+                if ($pos_x !== null && $pos_y !== null && $device_id !== null): ?>
+                    <button onclick="showTemperatureDetails('<?php echo $device_id; ?>')" class="button <?php echo $data['colorClass']; ?>" style="position: absolute; left: <?php echo $pos_x - 52.5; ?>px; top: <?php echo $pos_y - 125; ?>px;">
                         <?php echo htmlspecialchars($data['temp']); ?>
                     </button>
-                </form>
-            <?php endif; ?>
-        <?php endforeach; ?>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+        <div id="detailsArea" style="position: absolute; right: 0; top: 10px; width: 620px; height: 100%; overflow-y: auto; display: none;"></div>
     </div>
-
-    <form id="positionForm" method="POST">
-        <input type="hidden" id="dId" name="dId" value="">
-        <input type="hidden" id="pos_x" name="pos_x">
-        <input type="hidden" id="pos_y" name="pos_y">
-    </form>
+    <a href="positionMain.php" class="count-button">Devices without position: <?php echo $missingPosCount; ?></a>
 
     <script>
-        function showClickPosition(event) {
-            const x = event.clientX;
-            const y = event.clientY;
-
-            // Set the values in the form
-            document.getElementById('pos_x').value = x;
-            document.getElementById('pos_y').value = y;
-
-            if (deviceId) {
-                document.getElementById('dId').value = deviceId;
-
-                // Submit the form
-                document.getElementById('positionForm').submit();
-            }
+    function showTemperatureDetails(deviceId) {
+        var detailsArea = document.getElementById("detailsArea");
+        if (detailsArea.style.display === 'block' && lastClickedDeviceId === deviceId) {
+            detailsArea.style.display = 'none';
+            lastClickedDeviceId = null;
+        } else {
+            lastClickedDeviceId = deviceId;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "getTemperatureDetails.php?sensor=" + deviceId, true);
+            xhr.onload = function() {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    detailsArea.innerHTML = xhr.responseText;
+                    detailsArea.style.display = 'block';
+                }
+            };
+            xhr.send();
         }
+    }
+    var lastClickedDeviceId = null;
     </script>
-        <a href="positionMain.php" class="count-button">Devices without position: <?php echo $missingPosCount; ?></a>
-    </div>
 </body>
 </html>
